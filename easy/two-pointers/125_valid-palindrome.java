862596de3ac7ccc5215aa7bd00deb5f0c7f672e6
jared <jaredgold@proton.me>
2024-02-11 02:02:06 -0500

    add revised solution

--- easy/two-pointers/125_valid-palindrome.java
+++ easy/two-pointers/125_valid-palindrome.java
@@ -1,4 +1,4 @@
-//attemp 1, passed. not very efficient
+// attempt 1, passed. not very efficient
 
 class Solution {
     public boolean isPalindrome(String s) {
@@ -14,6 +14,30 @@ class Solution {
 
         for(int i = 0; i < max; i++) {
             if(cleanedCharArr[j] != cleanedCharArr[k]) return false;
+            j++;
+            k--;
+        }
+        return true;
+    }
+}
+
+// attempt 2
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] chArr = s.toCharArray();
+        int j = 0;
+        int k = chArr.length - 1;
+        for(char c: chArr) {
+            if(k<0 || j>=k) return true;
+            while(!Character.isLetterOrDigit(chArr[j])) {
+                if(j>=k) return true;
+                j++;
+            }
+            while(!Character.isLetterOrDigit(chArr[k])) {
+                if(k<=0) return true;
+                k--;
+            }
+            if(Character.toLowerCase(chArr[j]) != Character.toLowerCase(chArr[k])) return false;
             j++;
             k--;
         }
