bdf1ed92b50a39d156c6fa40035a48af32e1bca6
jared <jaredgold@proton.me>
2024-03-25 02:50:09 -0400

    update problem 1 w/ new submission

--- easy/arrays-and-hashing/1_two-sum.java
+++ easy/arrays-and-hashing/1_two-sum.java
@@ -16,3 +16,19 @@ class Solution {
 }
 // checked sol
 // https://leetcode.com/problems/two-sum/
+
+// 03/25/24
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+        
+    }
+}
