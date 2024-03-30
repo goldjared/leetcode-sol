c2358a7d4f1f4a415c5ceda5b627a6396d55aa16
jared <jaredgold@proton.me>
2024-03-30 19:44:05 -0400

    add 392

---
+++ easy/other/392_isSubsequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() == 0) return true;
+        int i = 0;
+        for(char c : t.toCharArray()) {
+            if(c == s.charAt(i)) {
+                i++;
+                if(i == s.length()) return true;
+            }
+
+        } 
+        return false;
+    }
+}
