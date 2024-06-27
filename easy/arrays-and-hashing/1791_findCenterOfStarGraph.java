7fa8bd17d46d28ba4f75a703effb94bb0e6f67ae
jared <jaredgold@proton.me>
2024-06-26 20:36:49 -0400

    add 1791

---
+++ easy/arrays-and-hashing/1791_findCenterOfStarGraph.java
@@ -0,0 +1,7 @@
+// 06/26/24, 9 min o(1)
+class Solution {
+    public int findCenter(int[][] edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+
+    }
+}
