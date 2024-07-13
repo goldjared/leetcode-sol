8a6ae81cf9b54e5a7f57ddf050d33a5984512176
jared <jaredgold@proton.me>
2024-07-13 14:38:26 -0400

    update 141

--- easy/linked-list/141_linkedListCycle.java
+++ easy/linked-list/141_linkedListCycle.java
@@ -14,3 +14,28 @@ public class Solution {
         
     }
 }
+
+// 07/13/24 30min
+//
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        if (head.next == null)
+            return false;
+
+        ListNode fast = head.next.next;
+
+        while (head.next != null) {
+            // fast can either be null, or .next.next
+            if (head == fast)
+                return true;
+            if (fast == null)
+                return false;
+            head = head.next;
+            if (fast.next != null)
+                fast = fast.next.next;
+        }
+        return false;
+    }
+}
