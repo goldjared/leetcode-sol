ea6197fe79080df5558ce170b929dd879d21c0f5
jared <jaredgold@proton.me>
2024-07-12 21:42:48 -0400

    add update 21

--- easy/linked-list/21_merge-two-sorted-lists.java
+++ easy/linked-list/21_merge-two-sorted-lists.java
@@ -27,3 +27,25 @@ class Solution {
         return main.next;
     }
 }
+
+// failure. 7/12/24 1:10, checked sol
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dumbo = new ListNode();
+        ListNode tail = dumbo;
+
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = l1 == null ? l2 : l1;
+        return dumbo.next;
+    }
+}
